c239e23a6ac712bf48ffcfda25f310c98e2973fe
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-09-06 12:43:38 -0400

    Installed tailwind css and started to make simple grid for projects

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,17 +3,44 @@ import AppBarCustom from '../components/navigation/AppBar'
 import './HomePage.css';
 
 function HomePage() {
+  const projectData = [
+    {
+      title: 'Project 1',
+      description: 'This is a brief description of Project 1.',
+    },
+    {
+      title: 'Project 2',
+      description: 'This is a brief description of Project 2.',
+    },
+    {
+      title: 'Project 3',
+      description: 'This is a brief description of Project 3.',
+    },
+    {
+      title: 'Project 4',
+      description: 'This is a brief description of Project 3.',
+    },
+  ];
+
+
   return (
     <>
       <AppBarCustom />
       {/* Body Div */}
       <div style={{marginTop: '3rem'}}>
-        <h1 style={{fontSize: 56}}>About Myself</h1>
+        <h1 className='text-[52px]'>About Myself</h1>
 
         <BioContent />
         {/* Projects Carousel Div */}
-        <div>
-                    
+        <div
+          className={`mx-auto grid grid-cols-1 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4`}
+        >
+          {projectData.map((project, index) => (
+            <div key={index} className='bg-black shadow-md rounded p-8'>
+              <h5 className='text-lg'>{project.title}</h5>
+              <p>{project.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
