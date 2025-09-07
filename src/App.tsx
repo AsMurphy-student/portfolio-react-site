3a7b1b606136711a31cdc7acb0a09cd90a40aa63
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-09-07 15:01:18 -0400

    Updated router and now testing another build

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename='/portfolio-react-site'>
+    <BrowserRouter basename='/portfolio-react-site/'>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/secondPage" element={<SecondPage />} />
