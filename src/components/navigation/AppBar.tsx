0ca372d5dcc67fd3e4b5f30f488fa0ddec05d3a0
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-19 18:48:45 -0400

    Formatted appbar.tsx

--- src/components/navigation/AppBar.tsx
+++ src/components/navigation/AppBar.tsx
@@ -13,6 +13,8 @@ const AppBarCustom = (props: { siteTheme: Theme }) => {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const buttonClassName = `${siteTheme.navButtonColor} ${siteTheme.navButtonHoverColor} ${siteTheme.navButtonTextColor} font-bold py-2 px-4`;
+
   return (
     <>
       <div className={`${siteTheme.accentColor} fixed top-0 left-0 right-0 flex justify-between px-4 h-10 w-screen`}
@@ -20,150 +22,111 @@ const AppBarCustom = (props: { siteTheme: Theme }) => {
         <span className='float-left flex items-center'>
           <Logo {...siteTheme} />
         </span>
-
         {
           width <= 680
-          ?
-          (
-            <span className='float-right mr-2 flex items-center'>
-              <div className="inline-flex">
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4 
-                rounded-l`}
-                onClick={() => {
-                  setMenuOpen(!menuOpen);
-                }}
-                >
-                  Menu
-                </button>
-              </div>
-            </span>
-          )
-          :
-          (
-            <span className='float-left mr-2 flex items-center'>
-              <div className="inline-flex">
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4 
-                rounded-l`}
-                onClick={() => {
-                  navigate('/');
-                  window.scrollTo({ top: 0, behavior: 'instant' });
-                }}
-                >
-                  Home
-                </button>
+            ?
+            (
+              <span className='float-right mr-2 flex items-center'>
+                <div className="inline-flex">
+                  <button
+                    className={`
+                    ${buttonClassName}
+                    rounded-l`}
+                    onClick={() => {
+                      setMenuOpen(!menuOpen);
+                    }}
+                  >
+                    Menu
+                  </button>
+                </div>
+              </span>
+            )
+            :
+            (
+              <span className='float-left mr-2 flex items-center'>
+                <div className="inline-flex">
+                  <button
+                    className={`${buttonClassName} rounded-l`}
+                    onClick={() => {
+                      navigate('/');
+                      window.scrollTo({ top: 0, behavior: 'instant' });
+                    }}
+                  >
+                    Home
+                  </button>
 
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4`}
-                onClick={() => {
-                  navigate('/#ProjectHeader');
-                  // Handles scrolling to project header when still on home
-                  const element = document.getElementById('ProjectHeader');
-                  if (element) {
-                      element.scrollIntoView({ behavior: 'smooth' });
-                  }
-                }}
-                >
-                  Projects
-                </button>
-                
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4 
-                rounded-r`}
-                onClick={() => {
-                  const element = document.getElementById('ContactInfo');
-                  if (element) {
-                      element.scrollIntoView({ behavior: 'smooth' });
-                  }
-                }}
-                >
-                  Contact Info
-                </button>
-              </div>
-            </span>
-          )
+                  <button
+                    className={buttonClassName}
+                    onClick={() => {
+                      navigate('/#ProjectHeader');
+                      // Handles scrolling to project header when still on home
+                      const element = document.getElementById('ProjectHeader');
+                      if (element) {
+                        element.scrollIntoView({ behavior: 'smooth' });
+                      }
+                    }}
+                  >
+                    Projects
+                  </button>
+
+                  <button
+                    className={`${buttonClassName} rounded-r`}
+                    onClick={() => {
+                      const element = document.getElementById('ContactInfo');
+                      if (element) {
+                        element.scrollIntoView({ behavior: 'smooth' });
+                      }
+                    }}
+                  >
+                    Contact Info
+                  </button>
+                </div>
+              </span>
+            )
         }
       </div>
-      { menuOpen && 
-      <div className={`${siteTheme.accentColor} fixed top-10 left-0 right-0 flex justify-between px-4 w-screen`}>
-        <span className='float-left mr-2 flex items-center'>
-              <div className="inline-flex">
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4 
-                rounded-l`}
+      {menuOpen &&
+        <div className={`${siteTheme.accentColor} fixed top-10 left-0 right-0 flex justify-between px-4 w-screen`}>
+          <span className='float-left mr-2 flex items-center'>
+            <div className="inline-flex">
+              <button
+                className={`${buttonClassName} rounded-l`}
                 onClick={() => {
                   navigate('/');
                   window.scrollTo({ top: 0, behavior: 'instant' });
                 }}
-                >
-                  Home
-                </button>
+              >
+                Home
+              </button>
 
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4`}
+              <button
+                className={buttonClassName}
                 onClick={() => {
                   navigate('/#ProjectHeader');
                   // Handles scrolling to project header when still on home
                   const element = document.getElementById('ProjectHeader');
                   if (element) {
-                      element.scrollIntoView({ behavior: 'smooth' });
+                    element.scrollIntoView({ behavior: 'smooth' });
                   }
                 }}
-                >
-                  Projects
-                </button>
-                
-                <button 
-                className={`
-                ${siteTheme.navButtonColor}
-                ${siteTheme.navButtonHoverColor}
-                ${siteTheme.navButtonTextColor}
-                font-bold 
-                py-2 px-4 
-                rounded-r`}
+              >
+                Projects
+              </button>
+
+              <button
+                className={`${buttonClassName} rounded-r`}
                 onClick={() => {
                   const element = document.getElementById('ContactInfo');
                   if (element) {
-                      element.scrollIntoView({ behavior: 'smooth' });
+                    element.scrollIntoView({ behavior: 'smooth' });
                   }
                 }}
-                >
-                  Contact Info
-                </button>
-              </div>
-            </span>
-      </div>
+              >
+                Contact Info
+              </button>
+            </div>
+          </span>
+        </div>
       }
     </>
   );
