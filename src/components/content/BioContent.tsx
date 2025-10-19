797cbd489493745ca44af1942b0457416efa8701
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-19 18:49:52 -0400

    Formatted biocontent.tsx

--- src/components/content/BioContent.tsx
+++ src/components/content/BioContent.tsx
@@ -1,25 +1,24 @@
 import BioPic from '../../assets/images/bio-pic-transparent.png';
 
 const BioContent = () => {
-    return (
-        <div className='grid grid-cols-1 xl:grid-cols-2 gap-16'>
-          
-          <div>
-            <img 
-            src={BioPic} alt="" 
-            className='rounded-xl mx-auto' />
-          </div>
-          <div className="flex items-center">
-            <p className='text-center text-3xl/12 p-4'>
-              My name is <u>Ashton Murphy</u>, and I am an up and coming Web Developer, 
-              graduating RIT (Rochester Institute of Technology). My proficiencies 
-              include <u>traditional web development</u> (HTML, CSS, Javascript/Typescript), <u>React Native 
-              with Typescript</u>, <u>Flutter</u>, along with backend tools such as <u>Node.js</u> and <u>Express.js</u>. 
-            </p>
-          </div>
+  return (
+    <div className='grid grid-cols-1 xl:grid-cols-2 gap-16'>
+      <div>
+        <img
+          src={BioPic} alt=""
+          className='rounded-xl mx-auto' />
+      </div>
+      <div className="flex items-center">
+        <p className='text-center text-3xl/12 p-4'>
+          My name is <u>Ashton Murphy</u>, and I am an up and coming Web Developer,
+          graduating RIT (Rochester Institute of Technology). My proficiencies
+          include <u>traditional web development</u> (HTML, CSS, Javascript/Typescript), <u>React Native
+            with Typescript</u>, <u>Flutter</u>, along with backend tools such as <u>Node.js</u> and <u>Express.js</u>.
+        </p>
+      </div>
 
-        </div>
-    );
+    </div>
+  );
 }
 
 export default BioContent;
