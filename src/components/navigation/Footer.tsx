7f52f2e83eb10c10041db7dc14504f0d2f7ef4a8
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-19 18:49:30 -0400

    Formatted footer.tsx

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,21 +1,21 @@
 import type { Theme } from "../../interfaces/theme";
 
 const Footer = (siteTheme: Theme) => {
-    return (
-        <div className={`${siteTheme.footerBackgroundColor} ${siteTheme.footerTextColor} min-h-64 font-[MartianMono]`}>
-          <div className="grid grid-cols-1 gap-6 xl:grid-cols-3 text-center xl:text-left">
-            <div id="ContactInfo">
-              <p>test</p>
-            </div>
-            <div>
-              <p>test</p>
-            </div>
-            <div>
-              <p>test</p>
-            </div>
-          </div>
+  return (
+    <div className={`${siteTheme.footerBackgroundColor} ${siteTheme.footerTextColor} min-h-64 font-[MartianMono]`}>
+      <div className="grid grid-cols-1 gap-6 xl:grid-cols-3 text-center xl:text-left">
+        <div id="ContactInfo">
+          <p>test</p>
         </div>
-    );
+        <div>
+          <p>test</p>
+        </div>
+        <div>
+          <p>test</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
